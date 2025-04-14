5f9952ea3b4b86253068f76096f23ee147377d2d
Adachi Masakazu <marscode48@gmail.com>
2025-04-14 09:09:17 +0900

    Relume&GPT/JavaScriptを追加

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import SwiperSlider from './modules/SwiperSlider.js';
+import HeroAnimation from './modules/HeroAnimation.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new SwiperSlider();
+  new HeroAnimation();
+});
