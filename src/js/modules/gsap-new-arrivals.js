e6b04eb467a916b97fec3569aa9eaab9a9733b20
Adachi Masakazu <marscode48@gmail.com>
2025-05-12 16:46:09 +0900

    refactor: gsapのcardのscaleを0.8に修正

--- src/js/modules/gsap-new-arrivals.js
+++ src/js/modules/gsap-new-arrivals.js
@@ -30,7 +30,7 @@ export class GsapNewArrivals {
         // カード全体のスケール＆フェードイン
         gsap.fromTo(
           card,
-          { scale: 0.9, opacity: 0 },
+          { scale: 0.8, opacity: 0 },
           { scale: 1, opacity: 1, duration: 0.5, ease: 'power3.out' }
         );
       }
